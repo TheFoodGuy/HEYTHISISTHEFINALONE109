b7b3a2ecfe5d1d1a59fc1cf359cc083314dbf6a2
David Liang <davidxliang@gmail.com>
2016-11-25 00:36:24 -0800

    Add files via upload

---
+++ Thread_Begin.h
@@ -0,0 +1,16 @@
+#ifndef THREADBEGIN_H
+#define THREADBEGIN_H
+
+#include "Threads.h"
+
+//#include "Thread.h"
+
+class Thread_Begin: public Threads{
+    public:
+        Thread_Begin();
+        void doThread(vector<Var*>const&);
+        Instruction *clone();
+        ~Thread_Begin();
+};
+
+#endif
---
+++ Threads.h
@@ -0,0 +1,18 @@
+#ifndef THREADS_H
+#define THREADS_H
+
+//#include "includes.h"
+#include "instruction.h"
+
+
+class Threads: public Instruction { //Thread class
+	public:
+		Threads();
+		virtual void doThread(vector < vector<Var*> >const&);
+		virtual Instruction* clone();
+		~Threads();
+};
+
+#endif 
+
+
