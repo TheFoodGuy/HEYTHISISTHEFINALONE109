3034f5008e0d6bb5158ca3f2fff23dad4a9e2349
Julius Mazer <jsmazer@ucsc.edu>
2016-11-25 16:02:14 -0800

    Commiting Thread_End.cpp and .h

--- Thread_End.cpp
+++ Thread_End.cpp
@@ -1,16 +1,26 @@
 #include "Thread_End.h"
 
+
 Thread_End::Thread_End(){
 
 }
 
 void Thread_End::doThread(vector < vector<Var*> >const&vec) {
+	tVec = vec;
 	cout << "Calling doThread in Thread_End" << endl;
 	vector <Var*> tempVec = vec.at(0);
 	cout << tempVec.at(0)->getString() << endl;
+
+	vector <Var*> tempVec2 = tVec.at(0);
+	cout << tempVec.at(0)->getString() << endl;
+	
+	Thread *t = new Thread_End();
+	t->start();
 }
 
 void *Thread_End::threadMainBody (void * arg){
+	cout << "In Thread_End threadMainBody" << endl;
+	//cout << tVec.at(0) << endl;
 	return nullptr;
 }
 
